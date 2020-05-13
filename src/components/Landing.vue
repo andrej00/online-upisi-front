abb3409a4be7d474bdc70c78e8b082f044681d79
Igor Vukic <igor.vukic8877@gmail.com>
2020-05-13 21:31:41 +0200

    naslovna_gotova

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
     <div class="naslovna_slika">
-      <b-container>
-        <b-row>
-          <b-col>
-            <div class="naslov">
-              <h2><b>Online upisi</b></h2>
-              <h3>U AKADEMSKU GODINU</h3>
-              <h1><b>2020./2021.</b></h1>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
+      <div class="container">
+        <div class="row" style="padding: 100px 0 20px 0">
+          <div class="col-md-2">
+            <img class="img-responsive" src="~@/assets/sum.png" alt="sum"
+              style="height: 70px; width: 150px"/>
+          </div>
+          <div class="col-md-2" style="margin-left: 40px">
+            <img class="img-responsive2" src="~@/assets/logofpmoz.png" alt="fpmoz"
+              style="height: 80px; width: 160px"/>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md" style="padding-top: 20px;">
+            <h1><b>Odaberi znanje!</b></h1>
+            <a class="btn btn-lg" role="button" style="color: white;" href="#">Nastavi</a>
+          </div>
+        </div>
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "Landing"
+  name: "Landing",
 };
 </script>
 
@@ -27,22 +36,40 @@ export default {
   background-image: url("~@/assets/naslovna.png");
   background-size: cover;
   background-position: center center;
-  height: 500px;
+  height: 600px;
+  position: relative;
 }
-.naslov{
-  color: #1a4283;
-  margin-top: 70px;
+.container{
+  font-family: "Open Sans", sans-serif;
+}
+.btn {
+  background-color: #1a4283fd !important;
 }
 
-@media screen and (max-width: 768px){
-  h1{
-    font-size: 2.0rem;
-  }
-  h2{
-    font-size: 1.5rem;
-  }
-  h3{
-    font-size: 1.25rem;
+h1{
+  color: #1a4283fd;
+}
+@media screen and (max-width: 990px) {
+  .img-responsive2{
+    margin-left: 45px;
   }
 }
-</style>>
+@media screen and (max-width: 768px) {
+  .col-md-2{
+    display: none;
+  }
+  .col-md{
+    margin-top: 90px;
+    text-align: center;
+    border-radius: 10px;
+    background-color: #ffffff73;
+    padding-bottom: 30px;
+  }
+  .naslovna_slika {
+  background-image: url("~@/assets/naslovna.png");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-position: right 25% bottom 25%;
+}
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,57 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info" fixed="top">
-      <b-navbar-brand href="#">Online upisi</b-navbar-brand>
+    <nav :class="{change_color: scrollPosition > 20}" class="navbar navbar-expand-lg navbar-dark fixed-top">
+      <a href="#" class="navbar-brand"></a>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">Naslovnica</b-nav-item>
-          <b-nav-item href="#">Forma za upis</b-nav-item>
-          <b-nav-item href="#">Informacije</b-nav-item>
-          <b-nav-item href="#">Razedbeni postupak</b-nav-item>
+          <b-nav-item href="#">{{navbar_text_1}}</b-nav-item>
+          <b-nav-item href="#">{{navbar_text_2}}</b-nav-item>
+          <b-nav-item href="#">{{navbar_text_3}}</b-nav-item>
+          <b-nav-item href="#">{{navbar_text_4}}</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
-    </b-navbar>
+    </nav>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data: function(){
+    return{
+      scrollPosition: null,
+      navbar_text_1: "NASLOVNICA",
+      navbar_text_2: "ODABERI ZNANJE",
+      navbar_text_3: "INFORMACIJE",
+      navbar_text_4: "RAZEDMENI POSTUPAK",
+    }
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+  }
 };
 </script>
 
 <style>
 .navbar {
-  transition: 1s;
   font-family: "Open Sans", sans-serif;
   box-shadow: 0 10px 7px -2px rgba(0, 0, 0, 0.2);
+  background-color: #1a428365 !important;
 }
 .navbar-brand {
   margin: 0px 0px 0px 30px;
 }
-.navbar.navbar-dark.bg-info {
-  background-color: #1a42837a !important;
-}
 .navbar-dark .navbar-nav .nav-link {
-  color: white !important;
-  margin: 0px 37px 0px 0px;
+  color: #fafafa !important;
+  margin: 0px 30px 0px 0px;
   font-weight: 500;
 }
 .navbar-dark .navbar-nav .nav-link::after {
@@ -51,5 +65,9 @@ export default {
 .navbar-dark .navbar-nav .nav-link:hover::after {
   width: 100%;
   transition: width 0.6s;
+}
+/*promjena navbar boje nakon event scroll*/
+.change_color {
+  background:#1a4283 !important;
 }
 </style>
